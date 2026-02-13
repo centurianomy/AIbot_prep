95d00c4800063a97c072be787a4364c49b6ca028
centurianomy <rawatchirag333@gmail.com>
2026-02-13 18:26:50 +0530

    updated

---
+++ sentencetransformer.py/3.py
@@ -0,0 +1,18 @@
+from sentence_transformers import SentenceTransformer, util #util is needed for similarity computation
+
+# Load model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Two sentences
+sentence1 = "Artificial intelligence is powerful."
+sentence2 = "AI is very powerful."
+
+# Convert both sentences into embeddings
+embedding1 = model.encode(sentence1)
+embedding2 = model.encode(sentence2)
+
+# Compute cosine similarity between the two embeddings
+similarity_score = util.cos_sim(embedding1, embedding2)
+
+# Print similarity score
+print("Similarity score:", similarity_score)
