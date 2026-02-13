0aa2ed6acdc0ab4bd6046f0f6e2a6f94c9453c45
centurianomy <rawatchirag333@gmail.com>
2026-02-13 19:16:07 +0530

    updated 3.py file

--- sentencetransformer.py/3.py
+++ sentencetransformer.py/3.py
@@ -5,7 +5,7 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # Two sentences
 sentence1 = "Artificial intelligence is powerful."
-sentence2 = "AI is very powerful."
+sentence2 = "I am from New Zealand."
 
 # Convert both sentences into embeddings
 embedding1 = model.encode(sentence1)
@@ -16,3 +16,5 @@ similarity_score = util.cos_sim(embedding1, embedding2)
 
 # Print similarity score
 print("Similarity score:", similarity_score)
+
+# o/p: Similarity score: tensor([[0.0202]])
